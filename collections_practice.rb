6ff9055b97ba8d05613ad15183a88d277c86f2e1
michael white <michaeljwhite@gmail.com>
2019-02-21 02:50:42 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -18,16 +18,21 @@ def swap_elements(array)
 end
 
 def reverse_array(array)
+  array.reverse
 end
 
 def kesha_maker(array)
+  array.collect {|elem| elem[2] = '$'}
 end
 
 def find_a(array)
+  array.select {|elem| elem.start_with?('a')}
 end
 
 def sum_array(array)
+  array.inject {|sum, n| sum + n}
 end
 
 def add_s(array)
+  array.each_with_index.collect {|elem, idx| elem = elem + 's' unless idx == 1}
 end

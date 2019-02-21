652d1c90fa4ba7a06933d5a971f54f6072e98126
michael white <michaeljwhite@gmail.com>
2019-02-21 02:34:40 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,13 +1,20 @@
 def sort_array_asc(array)
+  array.sort
 end
 
 def sort_array_desc(array)
+  array.reverse
 end
 
 def sort_array_char_count(array)
+  array.sort {|x, y| x.size <=> y.size }
 end
 
 def swap_elements(array)
+  temp = array[1]
+  array[1] = array[2]
+  array[2] = temp
+  array
 end
 
 def reverse_array(array)
